8791cbcdc2f3979939df875c5878ddbcf03fd2b4
karinaG343 <83309631+karinaG343@users.noreply.github.com>
2022-11-01 11:55:58 -0600

    Avance boton eliminar para pulseras

--- src/app/pulseras-modal/pulseras-modal.page.ts
+++ src/app/pulseras-modal/pulseras-modal.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 import { Pulseras, PulserasService } from '../services/pulseras.service';
 
 @Component({
@@ -9,12 +10,29 @@ import { Pulseras, PulserasService } from '../services/pulseras.service';
 export class PulserasModalPage implements OnInit {
   pulseras: Pulseras[];
 
-  constructor(private service: PulserasService) { }
+  constructor(private service: PulserasService, private alertCtrl: AlertController) { }
 
   ngOnInit() {
     this.service.getAll().subscribe(response => {
       this.pulseras = response;
     })
+  }
+
+  removePulsera(id: string){
+    this.alertCtrl.create({
+      header: 'Eliminar',
+      message: '¿Estas seguro que quieres eliminar la pulsera?',
+      buttons: [{
+        text: 'Si',
+        handler: () => {
+          this.service.remove(id).subscribe(() => {
+            this.pulseras = this.pulseras.filter(std => std.id !== id);
+          });
+        }
+      }, 
+    { text: 'No' } 
+  ]
+}).then(alertEl => alertEl.present());
   }
 
 }
